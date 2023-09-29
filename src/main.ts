818676e10f378af5fd6b0aa17cf40bb2d3589f43
linzui <yangminghe20@gmail.com>
2023-09-29 17:27:32 +0800

    admin骨架搭建

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,16 @@ import router from './router'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import pinia from './store'
 
 const app = createApp(App)
 
+//全局导入elemnt-plus的图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(pinia)
 app.use(router)
 app.use(ElementPlus)
--- src/views/admin.vue
+++ src/views/admin.vue
@@ -1,9 +1,41 @@
 <template>
-  <bookList></bookList>
+  <!-- <bookList></bookList> -->
+  <el-col :span="12">
+      <h5 class="mb-2">图书管理系统</h5>
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        
+      >
+        <el-menu-item index="1" >
+          <el-icon><House /></el-icon>
+          <span>主页</span>
+        </el-menu-item>
+        <el-menu-item index="2" >
+          <el-icon><User /></el-icon>
+          <span>用户管理</span>
+        </el-menu-item>
+        <el-menu-item index="3" >
+          <el-icon><Reading /></el-icon>
+          <span>图书管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><Notebook /></el-icon>
+          <span>图书借阅</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
 </template>
 
 <script setup lang="ts">
 import bookList from '@/components/bookList.vue'
+
+  // const handleOpen = (key: string, keyPath: string[]) => {
+  //   console.log(key, keyPath)
+  // }
+  // const handleClose = (key: string, keyPath: string[]) => {
+  //   console.log(key, keyPath)
+  // }
 
 </script>
 
